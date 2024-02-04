49e4116a69ab1167f374d4e2f558c8a715860586
TommyMcPhee <135555457+TommyMcPhee@users.noreply.github.com>
2024-02-03 18:54:32 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,1 @@
-#include "ofMain.h"
-#include "ofApp.h"
 
-int main() {
-	ofGLFWWindowSettings settings;
-	settings.setGLVersion(3, 2);
-	settings.setSize(1024, 768);
-	ofCreateWindow(settings);
-	ofSetFullscreen(true);
-	ofRunApp(new ofApp());
-}
